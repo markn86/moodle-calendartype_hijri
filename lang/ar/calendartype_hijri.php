353e820750bdd4a447ce0a5a28ad89008562844d
Shamim Rezaie <shamim@localhost.localdomain>
2013-11-06 21:10:22 +0330

    adding Arabic translation

---
+++ lang/ar/calendartype_hijri.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'calendartype_hijri', language 'ar'.
+ *
+ * @package calendartype_hijri
+ * @copyright 2008 onwards Foodle Group {@link http://foodle.org}
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['am'] = 'قبل الزوال';
+$string['am_caps'] = 'قبل الزوال';
+$string['name'] = 'الهجري';
+$string['month1'] = 'محرم';
+$string['month2'] = 'صفر';
+$string['month3'] = 'ربيع الأول';
+$string['month4'] = 'ربيع الثاني';
+$string['month5'] = 'جمادى الأول';
+$string['month6'] = 'جمادى الآخر';
+$string['month7'] = 'رجب';
+$string['month8'] = 'شعبان';
+$string['month9'] = 'رمضان';
+$string['month10'] = 'شوال';
+$string['month11'] = 'ذو القعدة';
+$string['month12'] = 'ذو الحجة';
+$string['pm'] = 'بعد الزوال';
+$string['pm_caps'] = 'بعد الزوال';
+$string['wday0'] = 'الأحد';
+$string['wday1'] = 'الإثنين';
+$string['wday2'] = 'الثلاثاء';
+$string['wday3'] = 'الأربعاء';
+$string['wday4'] = 'الخميس';
+$string['wday5'] = 'الجمعة';
+$string['wday6'] = 'السّبت';
+$string['weekday0'] = 'الأحد';
+$string['weekday1'] = 'الإثنين';
+$string['weekday2'] = 'الثلاثاء';
+$string['weekday3'] = 'الأربعاء';
+$string['weekday4'] = 'الخميس';
+$string['weekday5'] = 'الجمعة';
+$string['weekday6'] = 'السّبت';
+$string['name'] = 'الهجري';
+$string['pluginname'] = 'التقويم الهجري';
